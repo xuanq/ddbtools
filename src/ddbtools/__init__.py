c541432498a8f8afbc2a609ce2f61876c735c4bd
xuanqi <xuan.q@foxmail.com>
2024-08-08 14:57:19 +0800

    统一建表脚本

--- src/ddbtools/__init__.py
+++ src/ddbtools/__init__.py
@@ -1,2 +1,4 @@
 from loguru import logger
 logger.disable("ddbtools")
+from ddbtools.dbmanip import create_db
+from ddbtools.tablemanip import create_table
--- src/ddbtools/tablemanip.py
+++ src/ddbtools/tablemanip.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Literal
 import dolphindb as ddb
 from loguru import logger
+
 
 @dataclass
 class DbColumn:
@@ -16,6 +17,59 @@ def get_table_info(session: ddb.Session, db_name: str, table_name: str):
     return session.run("schema(db_table).colDefs")
 
 
+def create_table(
+    session: ddb.Session,
+    db_name: str,
+    table_name: str,
+    columns: DbColumn | List[DbColumn],
+    partition_by: str = None,
+    sortColumns: str = None,
+    keepDuplicates:Literal["ALL","LAST","FIRST"]=None,
+    sortKeyMappingFunction:str=None,
+):
+    if session.run(f"existsTable('{db_name}',`{table_name});"):
+        logger.info(
+            f"在数据库 {db_name} 下表 {table_name} 已存在，请删除重建或修改原表"
+        )
+        return f"在数据库 {db_name} 下表 {table_name} 已存在，请删除重建或修改原表"
+
+    if isinstance(columns, DbColumn):
+        columns = [columns]
+
+    col_strs = []
+    for col in columns:
+        col_str = f"{col.name} {col.dtype}"
+        patten = (isinstance(col.comment, str), isinstance(col.compress, str))
+        match patten:
+            case (True, True):
+                col_str = (
+                    col_str + f'[comment="{col.comment}",compress="{col.compress}"]'
+                )
+            case (True, False):
+                col_str = col_str + f'[comment="{col.comment}"]'
+            case (False, True):
+                col_str = col_str + f'[compress="{col.compress}"]'
+        col_strs.append(col_str)
+    cols_str = "\n".join(col_strs)
+
+    script = f"""
+        create table "{db_name}"."{table_name}"(
+            {cols_str}
+        )
+    """
+    if partition_by:
+        script = script + f"\npartitioned by {partition_by},"
+    if sortColumns:
+        script = script + f"\nsortColumns=[{sortColumns}],"
+    if keepDuplicates:
+        script = script + f"\nkeepDuplicates={keepDuplicates},"
+    if sortKeyMappingFunction:
+        script = script + f"\nsortKeyMappingFunction=[{sortKeyMappingFunction}]"
+    session.run(script)
+    logger.info(f"在数据库 {db_name} 下创建表 {table_name} 成功")
+    return f"在数据库 {db_name} 下创建表 {table_name} 成功"
+
+
 def create_dimensional_table(
     session: ddb.Session,
     db_name: str,
@@ -24,7 +78,9 @@ def create_dimensional_table(
     partition_by: str = None,
 ):
     if session.run(f"existsTable('{db_name}',`{table_name});"):
-        logger.info(f"在数据库 {db_name} 下表 {table_name} 已存在，请删除重建或修改原表")
+        logger.info(
+            f"在数据库 {db_name} 下表 {table_name} 已存在，请删除重建或修改原表"
+        )
         return f"在数据库 {db_name} 下表 {table_name} 已存在，请删除重建或修改原表"
 
     if isinstance(columns, DbColumn):
