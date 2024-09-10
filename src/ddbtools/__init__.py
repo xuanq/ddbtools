707fb081febe4559759b9c3009e0baf9f0dcb7a3
xuanqi <xuan.q@foxmail.com>
2024-09-10 13:56:01 +0800

    update ddbtools

--- src/ddbtools/__init__.py
+++ src/ddbtools/__init__.py
@@ -1,5 +1,5 @@
 from loguru import logger
 logger.disable("ddbtools")
 from ddbtools.dbmanip import create_db
-from ddbtools.tablemanip import create_table,get_table_info
+from ddbtools.tablemanip import create_table,get_table_info,DbColumn
 from ddbtools.crud import BaseCRUD,Filter,Comparator,DBDf
